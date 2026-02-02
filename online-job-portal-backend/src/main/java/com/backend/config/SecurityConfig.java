e5471572e1689b92a2f922e76445524b91d6dc21
AshishRajput18 <ashishrajput4999>
2026-02-02 15:34:47 +0530

    Solved problem cors

--- online-job-portal-backend/src/main/java/com/backend/config/SecurityConfig.java
+++ online-job-portal-backend/src/main/java/com/backend/config/SecurityConfig.java
@@ -43,7 +43,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173","https://*.vercel.app"));
+       configuration.setAllowedOrigins(List.of("http://localhost:5173","https://*.vercel.app"));
+
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);

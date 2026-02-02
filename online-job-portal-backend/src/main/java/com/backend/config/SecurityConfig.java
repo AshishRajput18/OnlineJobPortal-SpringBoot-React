c296ab464d2e84819734f2b974594eb2e3f36e2f
AshishRajput18 <ashishrajput4999>
2026-02-02 18:33:38 +0530

    Corrected vite url

--- online-job-portal-backend/src/main/java/com/backend/config/SecurityConfig.java
+++ online-job-portal-backend/src/main/java/com/backend/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -29,8 +28,7 @@ public class SecurityConfig {
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/api/**","/api/auth/**", "/api/register/**","/api/category/**","/api/users/**","/api/jobs/**","/api/applications/**").permitAll()
-                .requestMatchers(HttpMethod.POST, "/api/jobs/add/**").hasRole("EMPLOYER")
-
+                // .requestMatchers("/api/jobs/add/**").hasRole("EMPLOYER")
                 
                 .anyRequest().authenticated()
             )
